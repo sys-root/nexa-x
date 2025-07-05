96f7764b6c2e4a7bf9da261eeb7f0bc83f3b63ac
Pawan Choudhari <71498736+sys-root@users.noreply.github.com>
2025-07-05 16:48:39 +0530

    init
    ॥ श्री गणेशाय नमः ॥

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import { Button } from '@fluentui/react-components';
+
+function App() {
+  return (
+    <>
+      <Button appearance="primary">Click me</Button>
+    </>
+  );
+}
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { FluentProvider, webLightTheme } from "@fluentui/react-components";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <FluentProvider theme={webLightTheme}>
+      <App />
+    </FluentProvider>
+  </StrictMode>
+);
